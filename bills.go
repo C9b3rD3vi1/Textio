1d97efb400dc649b0423cedd46bf21abd8bf614d
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-10-26 16:03:01 +0300

    .

---
+++ bills.go
@@ -0,0 +1,52 @@
+package bills
+
+/*
+Assignment
+Fix the bugs in the monthlyBillIncrease and getBillForMonth functions.
+
+monthlyBillIncrease: Returns the increase in the bill from the previous to the current month. If the bill decreased, it should return a negative number.
+getBillForMonth: Returns the bill for the given month.
+It looks like whoever wrote the getBillForMonth function thought that they could pass in the bill parameter, update it inside the function, and that update would apply in the parent function (monthlyBillIncrease). They were wrong.
+
+Change the getBillForMonth function to explicitly return the bill for the given month, and be sure to capture that return value properly in the monthlyBillIncrease function.
+
+The function signature for getBillForMonth should only take 2 parameters once you're done.
+*/
+
+
+
+import (
+    "fmt"
+)
+
+
+// SOLUTION
+
+func monthlyBillIncrease(costPerSend, numLastMonth, numThisMonth int) int {
+
+
+	lastMonthBill := getBillForMonth(costPerSend, numLastMonth)
+	thisMonthBill := getBillForMonth(costPerSend, numThisMonth)
+	
+	return thisMonthBill - lastMonthBill
+}
+
+
+
+func getBillForMonth(costPerSend, messagesSent int) int {
+	bill := costPerSend * messagesSent
+
+	return bill
+}
+
+
+
+func bills() {
+	costPerSend := 10
+	numLastMonth := 50 // Messages sent last month
+	numThisMonth := 75 
+	
+	increase := monthlyBillIncrease(costPerSend, numLastMonth, numThisMonth)
+
+	fmt.Println("Monthly Bill Increase:", increase)
+}
